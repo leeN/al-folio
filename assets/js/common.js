b66646fe44085c74193abafa438f10782e748ee0
David Klein <david.klein@mpi-sp.org>
2026-05-09 23:24:40 +0200

    Updates

--- assets/js/common.js
+++ assets/js/common.js
@@ -5,11 +5,11 @@ $(document).ready(function () {
     $(this).parent().parent().find(".award.hidden.open").toggleClass("open");
     $(this).parent().parent().find(".bibtex.hidden.open").toggleClass("open");
   });
-  $("a.award").click(function () {
-    $(this).parent().parent().find(".abstract.hidden.open").toggleClass("open");
-    $(this).parent().parent().find(".award.hidden").toggleClass("open");
-    $(this).parent().parent().find(".bibtex.hidden.open").toggleClass("open");
-  });
+  // $("a.award").click(function () {
+  //   $(this).parent().parent().find(".abstract.hidden.open").toggleClass("open");
+  //   $(this).parent().parent().find(".award.hidden").toggleClass("open");
+  //   $(this).parent().parent().find(".bibtex.hidden.open").toggleClass("open");
+  // });
   $("a.bibtex").click(function () {
     $(this).parent().parent().find(".abstract.hidden.open").toggleClass("open");
     $(this).parent().parent().find(".award.hidden.open").toggleClass("open");
